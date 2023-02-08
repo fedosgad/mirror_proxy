c857b32cf895e30b5cf304e109f4391e36ead396
Fedor Gruzdev <fedosgad@mail.ru>
2023-02-08 16:17:52 +0300

    Change certificate validity period.

--- cert_generator/cert_generator.go
+++ cert_generator/cert_generator.go
@@ -52,12 +52,16 @@ func (cg *CertificateGenerator) GenChildCert(ips, names []string) (*tls.Certific
 func (cg *CertificateGenerator) genCertBytes(ips []string, names []string) (*rsa.PrivateKey, []byte, error) {
 	s, _ := rand.Prime(rand.Reader, 128)
 
+	// Certificate validity period should be less than 13 month.
+	// See https://stackoverflow.com/a/65239775
+	// Thanks to Johnny Bravo for the tip!
+
 	template := &x509.Certificate{
 		SerialNumber:          s,
 		Subject:               pkix.Name{Organization: []string{"mitmproxy"}},
 		Issuer:                pkix.Name{Organization: []string{"mitmproxy"}},
-		NotBefore:             time.Now().AddDate(-1, 0, 0),
-		NotAfter:              time.Now().AddDate(1, 0, 0),
+		NotBefore:             time.Now().AddDate(0, 0, -7),
+		NotAfter:              time.Now().AddDate(0, 0, 314),
 		BasicConstraintsValid: true,
 		IsCA:                  false,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
